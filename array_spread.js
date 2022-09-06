f1ac2d7dd2f1134c29616c1d9758613e30c73cab
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-07 02:01:25 +0600

    object 3

--- array_spread.js
+++ array_spread.js
@@ -3,6 +3,9 @@ const array1 = [4,5,6]
 // const obj = {
 //     ...array
 // }
-const array2 = [...array,...array1]
-console.log(array2)
+const array2 = [...array,...array1]  // array coping
+//console.log(array2)
 // console.log(obj)
+const obj1 = { foo: 'bar', x: 42 };
+const obj2 = { foo: 'baz', y: 13 };
+const obj3 ={...obj1}
