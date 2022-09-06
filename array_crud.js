72e4c8cf4a136b3e51246bf27eba5b4be4f83328
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-06 23:51:18 +0600

    crud

---
+++ array_crud.js
@@ -0,0 +1,12 @@
+const instructors = [
+    {
+      id: 'ee9efd79-92bb-4d9f-a4dc-d340e63739b7',
+      name: 'Ben Awad',
+      email: 'benawad@gmail.com',
+    },
+    {
+      id: '49350b1b-b450-418f-a840-c7aa53617a7f',
+      name: 'Hussein Nasser',
+      email: 'hussein@gmail.com',
+    }
+ ];
