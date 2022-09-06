8c028349fe5bbb925d3e26e7ed70a7fa383f969f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-07 00:58:38 +0600

    array crud

--- object_crud.js
+++ object_crud.js
@@ -1,3 +1,27 @@
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g
+  ,(c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+})};
 const obj ={
-    
+    "ee9efd79-92bb-4d9f-a4dc-d340e63739b7":{
+      id: 'ee9efd79-92bb-4d9f-a4dc-d340e63739b7',
+      name: 'Ben Awad',
+      email: 'benawad@gmail.com', 
+    },
+    '49350b1b-b450-418f-a840-c7aa53617a7f':{
+        id: '49350b1b-b450-418f-a840-c7aa53617a7f',
+        name: 'Hussein Nasser',
+        email: 'hussein@gmail.com',
+
+    }
 }
+newObj = {
+    id:uuid(),
+    name:"something",
+    email:"something@gmail.com"
+}
+instructors[newObj.id]=newObj
+console.log(instructors)
