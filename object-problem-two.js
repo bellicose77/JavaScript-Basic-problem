ccd58dec5741b9b6549fe86db210dd7304a542a7
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 19:45:32 +0600

    desturcting

--- object-problem-two.js
+++ object-problem-two.js
@@ -1,3 +1,14 @@
-const calCulateSalesTotal = () =>{
-    return [sales,total]
-}
+//  const calCulateSalesTotal = (sales) =>{
+
+//      return [sales,total]
+// }
+// const sales = [
+//     { item: 'PS4 Pro', stock: 3, original: 399.99 },
+//     { item: 'Xbox One X', stock: 1, original: 499.99, discount: 0.1 },
+//     { item: 'Nintendo Switch', stock: 4, original: 299.99 },
+//     { item: 'PS2 Console', stock: 1, original: 299.99, discount: 0.8 },
+//     { item: 'Nintendo 64', stock: 2, original: 199.99, discount: 0.65 }
+//   ];
+//   const {stock} = sales[0]
+//   console.log(stock)
+//   console.log(sales[0].stock)
