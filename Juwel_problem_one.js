a847cc96bf1843fa2a93071a1f60472111c26a88
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 18:20:51 +0600

    object desturcting

---
+++ Juwel_problem_one.js
@@ -0,0 +1,10 @@
+const obj ={
+    name: { first: 'Robert', middle: '', last: 'Smith' },
+    age: 25,
+    DOB: '-',
+    hobbies: [ 'running', 'coding', '-' ],
+    education: { highschool: 'N/A', college: 'Yale' }
+  
+}
+const {education} = obj
+console.log(education)
