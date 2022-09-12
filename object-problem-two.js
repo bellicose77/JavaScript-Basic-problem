6fe633871bd3e6bd21bd1ca3c9b11ce84a3887b9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 20:01:23 +0600

    I can add new key in object

--- object-problem-two.js
+++ object-problem-two.js
@@ -1,21 +1,15 @@
-//  const calCulateSalesTotal = (sales) =>{
+const sales = [
+    { item: 'PS4 Pro', stock: 3, original: 399.99 },
+    { item: 'Xbox One X', stock: 1, original: 499.99, discount: 0.1 },
+    { item: 'Nintendo Switch', stock: 4, original: 299.99 },
+    { item: 'PS2 Console', stock: 1, original: 299.99, discount: 0.8 },
+    { item: 'Nintendo 64', stock: 2, original: 199.99, discount: 0.65 }
+  ];
 
-//      return [sales,total]
-// }
-// const sales = [
-//     { item: 'PS4 Pro', stock: 3, original: 399.99 },
-//     { item: 'Xbox One X', stock: 1, original: 499.99, discount: 0.1 },
-//     { item: 'Nintendo Switch', stock: 4, original: 299.99 },
-//     { item: 'PS2 Console', stock: 1, original: 299.99, discount: 0.8 },
-//     { item: 'Nintendo 64', stock: 2, original: 199.99, discount: 0.65 }
-//   ];
-//   const {stock} = sales[0]
-//   console.log(stock)
-//   console.log(sales[0].stock)
 
 const newObj = {
     item: 'Xbox One X', 
-    stock: 1, 
+    stock: 2, 
     original: 499.99, 
     discount: 0.1 
 }
@@ -26,4 +20,14 @@ const calculateSalesTotal = ({stock,original,discount=0.0}) =>{
 
 }
 
-const result = calculateSalesTotal(newObj)
+const [sale,total] = calculateSalesTotal(newObj);
+//console.log(total);
+//  const updateData = {
+//      sale:sale,
+//      total:total
+//  }
+ updateObj = {
+    ...newObj,sale,total
+ }
+console.log("updateObj",updateObj);
+//console.log("first object",newObj)
