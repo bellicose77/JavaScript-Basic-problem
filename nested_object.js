b85e473673a0596207ca6206376937c82e50b3a9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-13 20:11:23 +0600

    nested object

---
+++ nested_object.js
@@ -0,0 +1,8 @@
+const obj ={
+    name: { first: 'Robert', middle: '', last: 'Smith' },
+    age: 25,
+    DOB: '-',
+    hobbies: [ 'running', 'coding', '-' ],
+    education: { highschool: 'N/A', college: 'Yale' }
+  
+}
