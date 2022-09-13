b05f1ac00d0d1bd5a53e3fbe53b5105b0eaa9f7d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-13 21:47:18 +0600

    nested object destructring done

--- nested_object.js
+++ nested_object.js
@@ -17,8 +17,8 @@ const obj ={
 }
   
 }
-const {name:{first,last},DOB,age,education:{college,love:{gfN,val:{ag}}}} =obj;
+const {name:{first,last},DOB,age,education:{college,love:{gfN,val:{ag}}},hobbies:[a,b]} =obj;
 console.log(`My first name is ${first} 
 and date of birth${DOB} and age is ${age},I in a collage of ${college}
-and my love is:${gfN} and her age is ${ag}
+and my love is:${gfN} and her age is ${ag} and she is loving ${b}
 `);
